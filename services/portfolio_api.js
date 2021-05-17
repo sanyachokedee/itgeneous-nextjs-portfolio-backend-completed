80259f62e7563d51807ed6dfdb2fe8572e2ba3d8
Nutthakit Anansuksakul <ing118199@gmail.com>
2021-05-17 09:33:23 +0700

    updated aportfolio_api.js to https://thawing-refuge-79448.herokuapp.com/t

--- services/portfolio_api.js
+++ services/portfolio_api.js
@@ -3,8 +3,8 @@ import axios from 'axios'
 const fetchPortfolio = async(path) => {
 
     // const url = `http://localhost:1337/${path}`   //local
-    // const url = `https://thawing-refuge-79448.herokuapp.com/${path}`
-    const url = `https://guarded-plains-40899.herokuapp.com/${path}`
+    const url = `https://thawing-refuge-79448.herokuapp.com/${path}`
+    // const url = `https://guarded-plains-40899.herokuapp.com/${path}`  //url ของอาจารย์ก็ทำงานได้ OK เอามาประยุกต์ใช้หลายเว๊ป แต่มาจากที่เดียว
     const res = await axios.get(url)
     return res.data
 }
