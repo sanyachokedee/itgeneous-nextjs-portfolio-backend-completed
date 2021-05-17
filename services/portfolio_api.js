4dd5b878ad44793f4eb2b5e9553452b712572d77
Nutthakit Anansuksakul <ing118199@gmail.com>
2021-05-17 09:26:14 +0700

    update portfolio_api url=samit

--- services/portfolio_api.js
+++ services/portfolio_api.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 const fetchPortfolio = async(path) => {
 
     // const url = `http://localhost:1337/${path}`   //local
-    const url = `https://thawing-refuge-79448.herokuapp.com/${path}`
+    // const url = `https://thawing-refuge-79448.herokuapp.com/${path}`
+    const url = `https://guarded-plains-40899.herokuapp.com/${path}`
     const res = await axios.get(url)
     return res.data
 }
